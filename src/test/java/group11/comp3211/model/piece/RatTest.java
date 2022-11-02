bdabdf6b532c7b624a5e2958a2804f537acffdff
Yunfei LIU <yunfei.liu@connect.polyu.hk>
2022-11-02 23:14:01 +0800

    update tests

---
+++ src/test/java/group11/comp3211/model/piece/RatTest.java
@@ -0,0 +1,32 @@
+package group11.comp3211.model.piece;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RatTest {
+    @Test
+    void move() {
+    }
+
+    @Test
+    void capture() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void canEqual() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+
+}
