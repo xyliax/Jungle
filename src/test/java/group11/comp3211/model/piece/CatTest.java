7b352d3055e339f8f8391b9241e26ce0ff0d570f
pyx <20083743d@connect.polyu.hk>
2022-11-03 04:13:40 +0800

    fix bugs in test

--- src/test/java/group11/comp3211/model/piece/CatTest.java
+++ src/test/java/group11/comp3211/model/piece/CatTest.java
@@ -1,10 +1,7 @@
 package group11.comp3211.model.piece;
 
 import group11.comp3211.model.Game;
-import group11.comp3211.model.Terrain;
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
 
 class CatTest {
 
@@ -12,7 +9,6 @@ class CatTest {
     void move() {
         Game game = new Game();
         Cat cat = new Cat();
-        game.getTerrain()[3][3] =
     }
 
     @Test
