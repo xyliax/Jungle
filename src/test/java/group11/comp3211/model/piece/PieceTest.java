657d80a6a1d4f8d67ca1b6c95d3ba7be9683e69e
hanjiaming <47519540+guomaimang@users.noreply.github.com>
2022-11-02 14:16:47 +0800

    update

---
+++ src/test/java/group11/comp3211/model/piece/PieceTest.java
@@ -0,0 +1,60 @@
+package group11.comp3211.model.piece;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PieceTest {
+
+    @Test
+    void capture() {
+    }
+
+    @Test
+    void getRank() {
+    }
+
+    @Test
+    void getRow() {
+    }
+
+    @Test
+    void getCol() {
+    }
+
+    @Test
+    void getPlayer() {
+    }
+
+    @Test
+    void setRank() {
+    }
+
+    @Test
+    void setRow() {
+    }
+
+    @Test
+    void setCol() {
+    }
+
+    @Test
+    void setPlayer() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void canEqual() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+}
