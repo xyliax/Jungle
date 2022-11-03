d41315c4ad20d840b37b896d941d891ed0107c3f
pyx <20083743d@connect.polyu.hk>
2022-11-03 20:56:32 +0800

    solve TA issue

--- src/main/java/group11/comp3211/JungleApplication.java
+++ src/main/java/group11/comp3211/JungleApplication.java
@@ -3,12 +3,12 @@ package group11.comp3211;
 import group11.comp3211.controller.GameManager;
 import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 public final class JungleApplication {
     public static void main(String[] args) {
-        log.info("abcde");
-        log.warn("abcde");
-        log.error("abcde");
+        if(System.console() == null) {
+            System.out.println("看readme运行");
+            System.exit(1);
+        }
         GameManager.getInstance().boot();
     }
 }
