fc6bea04d9ea50efd97713a71e33d78d3d6a150e
hanjiaming <47519540+guomaimang@users.noreply.github.com>
2022-11-04 14:45:20 +0800

    update

--- src/test/java/group11/comp3211/model/piece/DogTest.java
+++ src/test/java/group11/comp3211/model/piece/DogTest.java
@@ -10,16 +10,16 @@ import org.junit.Before;
 import org.junit.Test;
 
 /**
- * Test for Piece: Rat, Rank 1
+ * Test for Piece: Dog, Rank 3
  * Author: @HANJiaming
  * Create: 2022/11/03
  * Review: @LIUYunfei, @PEIYuxing
  */
-public class RatTest {
+public class DogTest {
 
     private PlayBoard playBoard;
     private Player player;
-    
+
     /**
      * 1. Create the playboard
      * 2. Create a player helding the piece
@@ -32,121 +32,119 @@ public class RatTest {
 
     /**
      * Ignore the Exception
-     * Test for Rat moving on play board
-     *     1. the Rat cannot move to out of the playboard
-     *  >> 2. the Rat cannot move more than one step
-     *  >> 3. the Rat is allowed to move for x or y
+     * Test for Dog moving on play board
+     *     1. the dog cannot move to out of the playboard
+     *  >> 2. the dog cannot move more than one step
+     *  >> 3. the dog is allowed to move for x or y
      *  >> 4. a failed move does not work and will change nothing
-     *     5. If there is an animal which ranks higher that it, Rat cannot move.
-     *     6. River test： Rat cannot go accross the river
-     *     7. If there is an animal which ranks lower that it, Rat can move
+     *     5. If there is an animal which ranks higher that it, dog cannot move.
+     *     6. Obstacle test： dog cannot go accross the obstacle
+     *     7. If there is an animal which ranks lower that it, dog can move
      */
     @SneakyThrows
     @Test
     public void moveTest1(){
-        Rat rat = new Rat(1, 2, player);
-        playBoard.put(rat);
-        rat.move(Direction.LEFT);
+        Dog dog = new Dog(1, 2, player);
+        playBoard.put(dog);
+        dog.move(Direction.LEFT);
         Landscape before = (Landscape) playBoard.get(1, 2);
         Landscape after = (Landscape) playBoard.get(1, 1);
-        Assert.assertTrue(after.getLoad() == rat && before.getLoad() == null);
+        Assert.assertTrue(after.getLoad() == dog && before.getLoad() == null);
     }
 
     /**
      * Ignore the Exception
-     * Test for Rat moving on play board
-     *  >> 2. the rat cannot move more than one step
-     *  >> 3. the rat is allowed to move for x or y
+     * Test for Dog moving on play board
+     *  >> 2. the dog cannot move more than one step
+     *  >> 3. the dog is allowed to move for x or y
      *  >> 4. a failed move does not work and will change nothing
      */
     @SneakyThrows
     @Test
     public void moveTest2(){
-        Rat rat = new Rat(1, 2, player);
-        playBoard.put(rat);
-        rat.move(Direction.UP);
+        Dog dog = new Dog(1, 2, player);
+        playBoard.put(dog);
+        dog.move(Direction.UP);
         Landscape before = (Landscape) playBoard.get(1, 2);
         Landscape after = (Landscape) playBoard.get(2, 2);
-        Assert.assertTrue(after.getLoad() == rat && before.getLoad() == null);
+        Assert.assertTrue(after.getLoad() == dog && before.getLoad() == null);
     }
 
     /**
      * Ignore the Exception
-     * Test for Rat moving on play board
-     *  >> 1. the rat cannot move to out of the playboard
-     *  >> 3. the rat is allowed to move for x or y
+     * Test for Dog moving on play board
+     *  >> 1. the dog cannot move to out of the playboard
+     *  >> 3. the dog is allowed to move for x or y
      *  >> 4. a failed move does not work and will change nothing
      */
     @SneakyThrows
     @Test
     public void moveTest3(){
-        Rat rat = new Rat(8, 0, player);
-        playBoard.put(rat);
-        rat.move(Direction.LEFT);
+        Dog dog = new Dog(8, 0, player);
+        playBoard.put(dog);
+        dog.move(Direction.LEFT);
         Landscape before = (Landscape) playBoard.get(8, 0);
         Landscape after = (Landscape) playBoard.get(8, 0);
-        Assert.assertTrue(after.getLoad() != rat && before.getLoad() == rat);
+        Assert.assertTrue(after.getLoad() != dog && before.getLoad() == dog);
     }
 
     /**
      * Ignore the Exception
-     * Test for Rat moving on play board
+     * Test for Dog moving on play board
      *  >> 4. a failed move does not work and will change nothing
-     *  >> 5. If there is an animal which ranks higher that it, rat cannot move
+     *  >> 5. If there is an animal which ranks higher that it, dog cannot move
      */
     @SneakyThrows
     @Test
     public void moveTest4(){
-        Rat rat = new Rat(3, 0, player);
-        playBoard.put(rat);
+        Dog dog = new Dog(3, 0, player);
+        playBoard.put(dog);
         Tiger tiger = new Tiger(2,0,player);
         playBoard.put(tiger);
 
-        rat.move(Direction.DOWN);
-        Landscape ratLoration = (Landscape) playBoard.get(3, 0);
-        Landscape tigerLoration = (Landscape) playBoard.get(2, 0);
-        Assert.assertTrue(ratLoration.getLoad() == rat && tigerLoration.getLoad() == tiger);
+        dog.move(Direction.DOWN);
+        Landscape dogLodogion = (Landscape) playBoard.get(3, 0);
+        Landscape tigerLodogion = (Landscape) playBoard.get(2, 0);
+        Assert.assertTrue(dogLodogion.getLoad() == dog && tigerLodogion.getLoad() == tiger);
     }
 
     /**
      * Ignore the Exception
-     * Test for Rat moving on play board
+     * Test for Dog moving on play board
      *  >> 4. a failed move does not work and will change nothing
-     *  >> 6. River test： rat can go accross the river
+     *  >> 6. Obstacle test： dog cannot go accross the obstacle
      */
 
     @SneakyThrows
     @Test
     public void moveTest5(){
-        Rat rat = new Rat(3, 0, player);
-        playBoard.put(rat);
+        Dog dog = new Dog(3, 0, player);
+        playBoard.put(dog);
 
-        rat.move(Direction.RIGHT);
+        dog.move(Direction.RIGHT);
         Landscape before = (Landscape) playBoard.get(3, 0);
         Landscape after = (Landscape) playBoard.get(3, 1);
-        Assert.assertTrue(before.getLoad() == rat && after.getLoad() != rat);
+        Assert.assertTrue(before.getLoad() == dog && after.getLoad() != dog);
     }
 
     /**
      * Ignore the Exception
-     * Test for Rat moving on play board
+     * Test for Dog moving on play board
      *  >> 4. a failed move does not work and will change nothing
-     *  >> 7. If there is an animal which ranks lower that it, rat can move
+     *  >> 7. If there is an animal which ranks lower that it, dog can move
      */
     @SneakyThrows
     @Test
     public void moveTest6(){
-        Rat rat = new Rat(1, 2, player);
+        Dog dog = new Dog(1, 2, player);
+        playBoard.put(dog);
+        Rat rat = new Rat(2,3,player);
         playBoard.put(rat);
-        Elephant elephant = new Elephant(2,3,player);
-        playBoard.put(elephant);
 
-        rat.move(Direction.UP);
+        dog.move(Direction.UP);
         Landscape before = (Landscape) playBoard.get(1, 2);
         Landscape after = (Landscape) playBoard.get(2, 3);
 
-        Assert.assertTrue(before.getLoad() == rat && after.getLoad() != rat);
+        Assert.assertTrue(before.getLoad() == dog && after.getLoad() != dog);
     }
-
-
 }
