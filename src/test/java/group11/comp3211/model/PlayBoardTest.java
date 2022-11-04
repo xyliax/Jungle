8def209da043823ca07b61832b67882a66733dd4
hanjiaming <47519540+guomaimang@users.noreply.github.com>
2022-11-04 18:43:35 +0800

    update

--- src/test/java/group11/comp3211/model/PlayBoardTest.java
+++ src/test/java/group11/comp3211/model/PlayBoardTest.java
@@ -1,10 +1,36 @@
 package group11.comp3211.model;
 
 import org.junit.Before;
+import org.junit.Test;
 
 public class PlayBoardTest {
+    private PlayBoard playBoard;
+    private Player player;
+
     @Before
     public void before() {
+        playBoard = new PlayBoard();
+        player = new Player("player");
+    }
 
+    @Test
+    public void testCanMove() {
+
+    }
+
+    @Test
+    public void testDoMove() {
+    }
+
+    @Test
+    public void testPut() {
+    }
+
+    @Test
+    public void testGet() {
+    }
+
+    @Test
+    public void testRegister() {
     }
 }
