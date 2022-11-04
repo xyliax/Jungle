0cb78d807445f24e24aa46f03aa1307c0bd00b74
pyx <20083743d@connect.polyu.hk>
2022-11-04 17:44:58 +0800

    Create empty test classes for model classes

--- src/main/java/group11/comp3211/model/piece/Piece.java
+++ src/main/java/group11/comp3211/model/piece/Piece.java
@@ -21,4 +21,5 @@ public abstract class Piece implements Movable {
         this.direction = Direction.STAY;
         this.player = player;
     }
+    
 }
---
+++ src/test/java/group11/comp3211/model/DirectionTest.java
@@ -0,0 +1,10 @@
+package group11.comp3211.model;
+
+import org.junit.Before;
+
+public class DirectionTest {
+    @Before
+    public void before() {
+
+    }
+}
