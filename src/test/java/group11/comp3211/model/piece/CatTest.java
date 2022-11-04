2ecf6d257fddc11616e99472c4bc25e76ace0349
hanjiaming <47519540+guomaimang@users.noreply.github.com>
2022-11-04 14:01:50 +0800

    update

--- src/test/java/group11/comp3211/model/piece/CatTest.java
+++ src/test/java/group11/comp3211/model/piece/CatTest.java
@@ -1,30 +1,150 @@
 package group11.comp3211.model.piece;
-
-import group11.comp3211.common.exceptions.LogicException;
 import group11.comp3211.model.Direction;
 import group11.comp3211.model.PlayBoard;
 import group11.comp3211.model.Player;
 import group11.comp3211.model.landscape.Landscape;
+
+import lombok.SneakyThrows;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+/**
+ * Test for Piece: Cat, Rank 2
+ * Author: @HANJiaming
+ * Create: 2022/11/03
+ * Review: @LIUYunfei
+ */
 public class CatTest {
     private PlayBoard playBoard;
     private Player player;
 
+    /**
+     * 1. Create the playboard
+     * 2. Create a player helding the piece
+     */
     @Before
-    public void init() {
+    public void before() {
         playBoard = new PlayBoard();
         player = new Player("player");
     }
 
+    /**
+     * Ignore the Exception
+     * Test for Cat moving on play board
+     *     1. the cat cannot move to out of the playboard
+     *  >> 2. the cat cannot move more than one step
+     *  >> 3. the cat is allowed to move for x or y
+     *  >> 4. a failed move does not work and will change nothing
+     *     5. If there is an animal which ranks higher that it, cat cannot move.
+     *     6. Obstacle test： cat cannot go accross the obstacle
+     *     7. If there is an animal which ranks lower that it, cat can move
+     */
+    @SneakyThrows
     @Test
-    public void move() throws LogicException {
+    public void moveTest1(){
         Cat cat = new Cat(1, 2, player);
         playBoard.put(cat);
         cat.move(Direction.LEFT);
         Landscape before = (Landscape) playBoard.get(1, 2);
         Landscape after = (Landscape) playBoard.get(1, 1);
-        assert after.getLoad() == cat && before.getLoad() == null;
+        Assert.assertTrue(after.getLoad() == cat && before.getLoad() == null);
     }
+
+    /**
+     * Ignore the Exception
+     * Test for Cat moving on play board
+     *  >> 2. the cat cannot move more than one step
+     *  >> 3. the cat is allowed to move for x or y
+     *  >> 4. a failed move does not work and will change nothing
+     */
+    @SneakyThrows
+    @Test
+    public void moveTest2(){
+        Cat cat = new Cat(1, 2, player);
+        playBoard.put(cat);
+        cat.move(Direction.UP);
+        Landscape before = (Landscape) playBoard.get(1, 2);
+        Landscape after = (Landscape) playBoard.get(2, 2);
+        Assert.assertTrue(after.getLoad() == cat && before.getLoad() == null);
+    }
+
+    /**
+     * Ignore the Exception
+     * Test for Cat moving on play board
+     *  >> 1. the cat cannot move to out of the playboard
+     *  >> 3. the cat is allowed to move for x or y
+     *  >> 4. a failed move does not work and will change nothing
+     */
+    @SneakyThrows
+    @Test
+    public void moveTest3(){
+        Cat cat = new Cat(8, 0, player);
+        playBoard.put(cat);
+        cat.move(Direction.LEFT);
+        Landscape before = (Landscape) playBoard.get(8, 0);
+        Landscape after = (Landscape) playBoard.get(8, 0);
+        Assert.assertTrue(after.getLoad() != cat && before.getLoad() == cat);
+    }
+
+    /**
+     * Ignore the Exception
+     * Test for Cat moving on play board
+     *  >> 4. a failed move does not work and will change nothing
+     *  >> 5. If there is an animal which ranks higher that it, cat cannot move
+     */
+    @SneakyThrows
+    @Test
+    public void moveTest4(){
+        Cat cat = new Cat(3, 0, player);
+        playBoard.put(cat);
+        Tiger tiger = new Tiger(2,0,player);
+        playBoard.put(tiger);
+
+        cat.move(Direction.DOWN);
+        Landscape catLocation = (Landscape) playBoard.get(3, 0);
+        Landscape tigerLocation = (Landscape) playBoard.get(2, 0);
+        Assert.assertTrue(catLocation.getLoad() == cat && tigerLocation.getLoad() == tiger);
+    }
+
+    /**
+     * Ignore the Exception
+     * Test for Cat moving on play board
+     *  >> 4. a failed move does not work and will change nothing
+     *  >> 6. Obstacle test： cat cannot go accross the obstacle
+     */
+
+    @SneakyThrows
+    @Test
+    public void moveTest5(){
+        Cat cat = new Cat(3, 0, player);
+        playBoard.put(cat);
+
+        cat.move(Direction.RIGHT);
+        Landscape before = (Landscape) playBoard.get(3, 0);
+        Landscape after = (Landscape) playBoard.get(3, 1);
+        Assert.assertTrue(before.getLoad() == cat && after.getLoad() != cat);
+    }
+
+    /**
+     * Ignore the Exception
+     * Test for Cat moving on play board
+     *  >> 4. a failed move does not work and will change nothing
+     *  >> 7. If there is an animal which ranks lower that it, cat can move
+     */
+    @SneakyThrows
+    @Test
+    public void moveTest6(){
+        Cat cat = new Cat(1, 2, player);
+        playBoard.put(cat);
+        Rat rat = new Rat(2,3,player);
+        playBoard.put(rat);
+
+        cat.move(Direction.UP);
+        Landscape before = (Landscape) playBoard.get(1, 2);
+        Landscape after = (Landscape) playBoard.get(2, 3);
+
+        Assert.assertTrue(before.getLoad() == cat && after.getLoad() != cat);
+    }
+
 }
